9f627c7466e1f7ddab9882cde54ccc934c66e66e
Caio Barilli <caiobarilli@gmail.com>
2023-12-29 21:06:40 -0300

    fix: fix names

--- cms/wp-content/themes/custom_theme_woocommerce/assets/scripts/main.js
+++ cms/wp-content/themes/custom_theme_woocommerce/assets/scripts/main.js
@@ -34,7 +34,7 @@ $.when($.ready).then(function () {
     /**
      * Remove Price for not logged in users
      */
-    let elementosPrice = document.querySelectorAll('.Custom Theme_product_infos .price');
+    let elementosPrice = document.querySelectorAll('.Custom_Theme_product_infos .price');
     if (elementosPrice) {
         elementosPrice.forEach(function (elemento) {
             elemento.remove();
--- cms/wp-content/themes/custom_theme_woocommerce/inc/woocommerce.php
+++ cms/wp-content/themes/custom_theme_woocommerce/inc/woocommerce.php
@@ -3,8 +3,8 @@
 /**
  * Add WooCommerce support
  */
-if (!function_exists('Custom Theme_add_woocommerce_support')) {
-    function Custom Theme_add_woocommerce_support()
+if (!function_exists('Custom_Theme_add_woocommerce_support')) {
+    function Custom_Theme_add_woocommerce_support()
     {
         add_theme_support('woocommerce');
         add_theme_support('wc-product-gallery-zoom');
@@ -12,29 +12,26 @@ if (!function_exists('Custom Theme_add_woocommerce_support')) {
         add_theme_support('wc-product-gallery-slider');
     }
 }
-
-add_action('after_setup_theme', 'Custom Theme_add_woocommerce_support', 5);
+add_action('after_setup_theme', 'Custom_Theme_add_woocommerce_support', 5);
 
 /**
  * Hide price for unlogged users
- * @hooked hide_price
- * @return bool
  */
-if (!function_exists('Custom Theme_product_infos')) {
-    function Custom Theme_product_infos_open()
+if (!function_exists('Custom_Theme_product_infos_open')) {
+    function Custom_Theme_product_infos_open()
     {
         if (!is_user_logged_in()) {
-            echo '<div class="Custom Theme_product_infos">';
+            echo '<div class="Custom_Theme_product_infos">';
         }
     }
 }
-if (!function_exists('Custom Theme_hide_price')) {
-    function Custom Theme_product_infos_close()
+if (!function_exists('Custom_Theme_product_infos_close')) {
+    function Custom_Theme_product_infos_close()
     {
         if (!is_user_logged_in()) {
             echo '</div>';
         }
     }
 }
-add_action('woocommerce_shop_loop_item_title', 'Custom Theme_product_infos_open', 0);
-add_action('woocommerce_after_shop_loop_item', 'Custom Theme_product_infos_close', 10);
+add_action('woocommerce_shop_loop_item_title', 'Custom_Theme_product_infos_open', 0);
+add_action('woocommerce_after_shop_loop_item', 'Custom_Theme_product_infos_close', 10);
